36da7fcde9d89b6df21cdf2cb1514dcc7d25328f
Oleg Nikolaev <o.nikolaev.nsk@gmail.com>
2025-06-16 14:40:37 +0500

    feat: focus password input on eye click

--- apps/front/src/shared/ui/input/password-input.tsx
+++ apps/front/src/shared/ui/input/password-input.tsx
@@ -1,5 +1,11 @@
 import { EyeNoneIcon, EyeOpenIcon } from "@radix-ui/react-icons";
-import { type ComponentProps, useState } from "react";
+import {
+  type ComponentProps,
+  type ComponentRef,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import { Input } from "./input";
 
 type PasswordInputProps = Omit<ComponentProps<typeof Input>, "type">;
@@ -7,9 +13,31 @@ type PasswordInputProps = Omit<ComponentProps<typeof Input>, "type">;
 export const PasswordInput = (props: PasswordInputProps) => {
   const [inputType, setInputType] = useState<"password" | "text">("password");
 
+  const inputRef = useRef<ComponentRef<"input">>(null);
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: should be called on inputType change
+  useLayoutEffect(() => {
+    inputRef.current?.setSelectionRange(
+      inputRef.current.value.length,
+      inputRef.current.value.length,
+    );
+  }, [inputType]);
+
+  const focusInput = () => {
+    inputRef.current?.focus();
+  };
+
   return (
     <Input
       {...props}
+      ref={(e) => {
+        inputRef.current = e;
+        if (typeof props.ref === "function") {
+          props.ref(e);
+        } else if (props.ref) {
+          props.ref.current = e;
+        }
+      }}
       type={inputType}
       icon={
         <button
@@ -19,6 +47,7 @@ export const PasswordInput = (props: PasswordInputProps) => {
           onClick={() => {
             setInputType((prev) => (prev === "password" ? "text" : "password"));
           }}
+          onFocus={focusInput}
         >
           {inputType === "password" ? (
             <EyeOpenIcon className="pointer-events-none" />
